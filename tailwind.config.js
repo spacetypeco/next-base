01accebd86292522eaabf63fee405ba9cfb36dc0
Kevin Yeh <kevinyeah@utexas.edu>
2024-07-17 23:13:05 -0400

    remove tailwind warnings

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,10 +45,5 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: tailwindColors,
-    },
-  },
   plugins: [],
 };
